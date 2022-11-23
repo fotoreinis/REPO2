5ff8dd53a82d2eb1d58ce21370ccc3aaddafc8b9
Reinis <reinis.melioranskis@va.lv>
2022-11-23 09:12:32 +0000

    config

--- naked.py
+++ naked.py
@@ -6,14 +6,28 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
+#OPEN & READ CONFIG FILE
+
+try:
+    config = ConfigParser()
+    config.read('config.ini')
+
+    nasa_api_key = config.get('nasa', 'api_key')
+    nasa_api_url = config.get('nasa', 'api_url')
+except:
+    logger.exception('')
+#logger.info('DONE')
+print('DONE')
+
 # NASA API KEYS
-nasa_api_key = "z8g9dab0bQthQ6WkRd3pr3LOe2uLZHiGAhv9nl8F"
-nasa_api_url = "https://api.nasa.gov/neo/"
+#nasa_api_key = "z8g9dab0bQthQ6WkRd3pr3LOe2uLZHiGAhv9nl8F"
+#nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
 dt = datetime.now()
@@ -42,11 +56,10 @@ if r.status_code == 200:
 		if ast_count > 0:
 			for val in json_data['near_earth_objects'][request_date]:
 				if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
-#NAME OF ASTEROID		
-			tmp_ast_name = val['name']
-#URL OF ASTEROID DSC		
-			tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
-#ASTEROID DIAMETER		
+#NAME OF ASTEROID
+					tmp_ast_name = val['name']
+					tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
+#ASTEROID DIAMETER
 			if 'kilometers' in val['estimated_diameter']:
 						if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
 							tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
@@ -54,12 +67,11 @@ if r.status_code == 200:
 						else:
 							tmp_ast_diam_min = -2
 							tmp_ast_diam_max = -2
-					else:
+else:
 						tmp_ast_diam_min = -1
 						tmp_ast_diam_max = -1
 #UNSAFE ASTEROID
-					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
-
+tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
 					if len(val['close_approach_data']) > 0:
 						if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
 							tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
@@ -91,7 +103,7 @@ if r.status_code == 200:
 					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
 					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
 					print("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
-					
+
 					# CHECK ASTEROID SAFE OR UNSAFE \\ Adding asteroid data to the corresponding array
 					if tmp_ast_hazardous == True:
 						ast_hazardous.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
